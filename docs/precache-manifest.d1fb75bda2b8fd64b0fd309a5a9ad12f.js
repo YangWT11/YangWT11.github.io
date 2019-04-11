d1e98bd13778258b8404ce3b0c124e2526c74233
YangWT11 <627088152@qq.com>
2019-04-11 20:55:49 +0800

    try to build the vue project

---
+++ docs/precache-manifest.d1fb75bda2b8fd64b0fd309a5a9ad12f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "00d5f50d576cd2437cf4",
+    "url": "/js/chunk-vendors.492db40c.js"
+  },
+  {
+    "revision": "e4b4cd31c0cf742f71cb",
+    "url": "/js/app.41840cf3.js"
+  },
+  {
+    "revision": "d7c849ad540c6c2a0455",
+    "url": "/js/about.ad763791.js"
+  },
+  {
+    "revision": "794acec9ae2d63dabd06bc7eb639a37e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e4b4cd31c0cf742f71cb",
+    "url": "/css/app.48f5c24c.css"
+  }
+];
