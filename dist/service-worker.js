da9dfad6a99dca4aad142a759a3270deb9d70072
YangWT11 <627088152@qq.com>
2019-04-13 13:58:33 +0800

    try to use the default method to deploy site

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "dist/precache-manifest.03c8e5a1b44223e27ff4bd09ed408f9e.js"
+  "dist/precache-manifest.d10abf487aa4c351c9a1903bb83743d8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "blog"});
